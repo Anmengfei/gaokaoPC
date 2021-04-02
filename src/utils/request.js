96159b3a54da4149cfa71fe597e357b73cc83d8c
Hi_Rick <1185696945@qq.com>
2021-04-02 16:31:41 +0800

    重新提交

---
+++ src/utils/request.js
@@ -0,0 +1,27 @@
+import axios from 'axios'
+import qs from 'qs'
+import {getToken} from "./auth";
+// 创建axios实例
+const service = axios.create({
+  baseURL: 'https://www.zytb.top/NEMT/gk/'
+})
+// request拦截器
+service.interceptors.request.use(config => {
+  // 在发送请求之前做些什么
+  // config['headers']['Content-Type'] = 'application/x-www-form-urlencoded'
+  // if (config.method === 'POST' || config.method === 'post') {
+  //   config.data = qs.stringify(config.data)
+  // }
+  config['headers']['Content-Type'] = 'application/x-www-form-urlencoded'
+  console.log()
+  if (localStorage.getItem('token')) {
+    config.headers['token'] = getToken()
+  }
+  return config
+}, error => {
+  // 对请求错误做些什么
+  Promise.reject(error)
+  console.info(error)
+})
+
+export default service
